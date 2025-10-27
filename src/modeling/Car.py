2f273ce254eeffad9625356cb514f4d2d479439a
xingmei <dingxingmei.dxm@gmail.com>
2025-10-27 22:50:49 +0000

    add modeling

--- src/modeling/Car.py
+++ src/modeling/Car.py
@@ -6,3 +6,9 @@ class Car:
     yaw: float
     velocity: float = 0.0
     steer: float = 0.0
+
+    def __init__(self, x: float, y: float, yaw: float):
+        self.x = x
+        self.y = y
+        self.yaw = yaw
+        
